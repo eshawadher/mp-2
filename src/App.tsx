e133c88757f06b71af08027c0460dbec747ddfb0
Esha Wadher <eshaaw@bu.edu>
2025-09-29 22:56:02 -0400

    fixed issues with char variable so that i can deploy with no issues

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import {useEffect, useState} from "react";
 import type {Character} from "./interfaces/Character.ts";
 
+
 const ParentDiv=styled.div`
     width: 80vw;
     margin: auto;
@@ -20,7 +21,7 @@ export default function App(){
             const rawData = await(await fetch("https://thronesapi.com/api/v2/Characters")).json();
             console.log(rawData);
             const results: Character[] = [];
-            rawData.map((char)=>{
+            rawData.map((char:{ id: number; fullName: string; imageUrl: string; title: string; status:string })=>{
                 const character: Character  = {
                     id: char.id,
                     full_name: char.fullName,
